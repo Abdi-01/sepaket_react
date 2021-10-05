3c496b787c4af5774cefe1a23c01daa13fdc54c7
fauziUI <fauziUI@gmail.com>
2021-10-05 21:50:54 +0700

    FE-Fauzi-001 : Tampilan Login
    Tampilan login tanpa back end

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import ReduxThunk from "redux-thunk"
+import rootReducer from './redux/reducers'
+
+const store = createStore(rootReducer,applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+
   document.getElementById('root')
 );
 
